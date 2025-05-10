811b4266e216c45a1a912841521d91ad4234737e
M L Store <mlstore@Ms-MacBook-Pro.local>
2025-05-11 01:22:43 +0300

    Update published website

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1994195430"
+    serviceWorkerVersion: "1723641512"
   }
 });
